7a8ffb139e98c546d66f3d0e90b9ff2f9c8b7d74
= <jirobassik@tut.by>
2023-04-07 20:48:46 +0300

    Add request error send to user

--- HTTP Server/read_http.py
+++ HTTP Server/read_http.py
@@ -11,13 +11,42 @@ test_str = (
 
 
 class HTTPServer:
+    def __init__(self, port, host=""):
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def start_server(self):
+        with socket() as sk:
+            sk.bind(
+                (
+                    self.host,
+                    self.port,
+                )
+            )
+            sk.listen(4)
+            while True:
+                self.connection, address = sk.accept()
+                with self.connection:
+                    logger.info(f"Connect address {address}")
+                    try:
+                        self.read_http(self.connection)
+                    except HTTPError as error:
+                        logger.warning(error)
+                    while data := self.connection.recv(1024):
+                        logger.info(f"Client message {data} {type(data)}")
+                        self.connection.sendall(b"Hello client socket")
+                logger.info(f"Connection {address} lost")
+
     def read_http(self, http: socket):
         http_file = http.makefile("rb")
         http_list = self.read_byte_file(http_file)
         method, target, version_http = self.read_request_line(http_list.pop(0))
-        logger.debug(f'Method: {method}, Target: {target}, Version HTTP: {version_http}')
+        logger.debug(
+            f"Method: {method}, Target: {target}, Version HTTP: {version_http}"
+        )
         headers = self.read_header_lines(http_list)
-        logger.debug(f'Headers: {headers}')
+        logger.debug(f"Headers: {headers}")
 
     @staticmethod
     def read_byte_file(http_byte: BinaryIO) -> list[str]:
@@ -26,22 +55,23 @@ class HTTPServer:
             headers.append(line.decode("iso-8859-1").replace("\r\n", ""))
         return headers
 
-    @staticmethod
-    def read_request_line(req_line: str) -> tuple[str, str, str]:
+    def read_request_line(self, req_line: str) -> tuple[str, str, str]:
         split_request = req_line.split()
         if len(split_request) != 3:
-            raise Exception("400 Bad request")
+            raise HTTPError("400", "Bad request", connection_host=self.connection)
         method, target, version_http = split_request
         if method not in (
             "GET",
             "POST",
             "OPTIONS",
         ):
-            raise Exception(
-                "405 Method Not Allowed"
+            raise HTTPError(
+                "405", "Method Not Allowed", connection_host=self.connection
             )  # Так же можно добавить подсказку о поддерживаемых методах
         if version_http != "HTTP/1.1":
-            raise Exception("505 HTTP Version Not Supported")
+            raise HTTPError(
+                "505", "HTTP Version Not Supported", connection_host=self.connection
+            )
         return method, target, version_http
 
     @staticmethod
@@ -55,4 +85,40 @@ class HTTPServer:
 
 
 class HTTPError(Exception):
-    pass
+    def __init__(self, error, error_message, connection_host, message=None):
+        self.num_error = error
+        self.error_message = error_message
+        self.message = message
+        self.connection_host = connection_host
+        Request(self.num_error, self.error_message, connection_host).send_request()
+
+    def __str__(self):
+        return f"Num error: {self.num_error}, Error message: {self.error_message}"
+
+    def send_error(self):
+        pass
+
+
+class Request:
+    def __init__(
+        self,
+        status_code,
+        message,
+        connection_host: socket,
+        version="HTTP/1.1",
+        **headers,
+    ):
+        self.version = version
+        self.status_code = status_code
+        self.message = message
+        self.connection_host = connection_host
+        self.headers = headers or {}
+
+    def send_request(self):
+        #  Почему-то сам отправляет
+        reqeust_file = self.connection_host.makefile("wb")
+        request_line = f"{self.version} {self.status_code} {self.message}\r\n"
+        reqeust_file.write(request_line.encode("iso-8859-1"))
+        reqeust_file.write(b"\r\n")
+        reqeust_file.flush()
+        reqeust_file.close()
--- HTTP Server/test_net.py
+++ HTTP Server/test_net.py
@@ -1,52 +1,8 @@
-from socket import socket
 from typing import Final
 from read_http import HTTPServer
-from logging_conf import logger
 
-HOST: Final = '127.0.0.1'
+HOST: Final = "127.0.0.1"
 PORT: Final = 54879
 
-#
-# def load_page_from_request(request_data: str):
-#     hdrs = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-#     hdrs_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-#     print(f"request_Data:{request_data}")
-#     path = request_data.split(' ')[1]
-#     print(path)
-#     try:
-#         with open('view' + path, 'rb') as file:
-#             response = file.read()
-#         return hdrs.encode('utf-8') + response
-#     except (FileNotFoundError, PermissionError):
-#         response = 'File not found'.encode('utf-8')
-#         return hdrs_404.encode('utf-8') + response
-#
-#
-# with socket() as sk:
-#     sk.bind((HOST, PORT,))
-#     sk.listen(4)
-#     while True:
-#         connection, address = sk.accept()
-#         with connection:
-#             print(f"Connect address {address}")
-#             while data := connection.recv(1024):
-#                 hdrs = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
-#                 print(f"Client message {data}")
-#                 connection.sendall(b'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n')
-#
-#                 # connection.send(hdrs.encode('utf-8') + "Lox".encode('utf-8'))
-#         print(f"Connection {address} lost")
-
-a = HTTPServer()
-with socket() as sk:
-    sk.bind((HOST, PORT,))
-    sk.listen(4)
-    while True:
-        connection, address = sk.accept()
-        with connection:
-            logger.info(f'Connect address {address}')
-            a.read_http(connection)
-            while data := connection.recv(1024):
-                logger.info(f"Client message {data} {type(data)}")
-                connection.sendall(b'Hello client socket')
-        logger.info(f"Connection {address} lost")
+a = HTTPServer(port=PORT, host=HOST)
+a.start_server()
